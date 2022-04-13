7b879ae77e24f828496d1911524cd33bff2d26d8
Anthony Kim <fbssaejun@gmail.com>
2022-04-13 00:46:15 -0400

    print varaible with string concatenation

---
+++ day-2.py
@@ -0,0 +1,17 @@
+'''
+
+Challenge
+
+Modify the name on Dot’s passport so it can be recognized by passport control. Which combination of string functions will give us the right results?
+
+name: Dot
+
+Required format: #DOT!
+
+'''
+
+# Variable
+name = '  Dot  '
+
+# Solution
+'#' + name.strip().upper() + '!' # => #DOT!
